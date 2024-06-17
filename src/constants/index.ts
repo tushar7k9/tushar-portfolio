91bc9cf3d64d029f2f58123c3de99a608d0b284a
Tushar <tushar@Tushars-MacBook-Pro.local>
2024-06-17 13:45:41 +0530

    updated projects list

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -256,64 +256,64 @@ export const projects = [
         link: 'https://tushhkashyap.github.io/Sorting-Visualizer/',
         linkType: 'Live Link',
     },
-    {
-        iconUrl: pricewise,
-        theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-        description: [
-            'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.'
-        ],
-        link: 'https://github.com/adrianhajdin/pricewise',
-        linkType: 'Live Link',
-    },
-    {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description: [
-            'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.'
-        ],
-        link: 'https://github.com/adrianhajdin/threads',
-        linkType: 'Live Link',
-    },
-    {
-        iconUrl: car,
-        theme: 'btn-back-blue',
-        name: 'Car Finding App',
-        description: [
-            'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.'
-        ],
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-        linkType: 'Live Link',
-    },
-    {
-        iconUrl: snapgram,
-        theme: 'btn-back-pink',
-        name: 'Full Stack Instagram Clone',
-        description: [
-            'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.'
-        ],
-        link: 'https://github.com/adrianhajdin/social_media_app',
-        linkType: 'Live Link',
-    },
-    {
-        iconUrl: estate,
-        theme: 'btn-back-black',
-        name: 'Real-Estate Application',
-        description: [
-            'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.'
-        ],
-        link: 'https://github.com/adrianhajdin/projects_realestate',
-        linkType: 'Live Link',
-    },
-    {
-        iconUrl: summiz,
-        theme: 'btn-back-yellow',
-        name: 'AI Summarizer Application',
-        description: [
-            'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.'
-        ],
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
-        linkType: 'Live Link',
-    }
+    // {
+    //     iconUrl: pricewise,
+    //     theme: 'btn-back-red',
+    //     name: 'Amazon Price Tracker',
+    //     description: [
+    //         'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.'
+    //     ],
+    //     link: 'https://github.com/adrianhajdin/pricewise',
+    //     linkType: 'Live Link',
+    // },
+    // {
+    //     iconUrl: threads,
+    //     theme: 'btn-back-green',
+    //     name: 'Full Stack Threads Clone',
+    //     description: [
+    //         'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.'
+    //     ],
+    //     link: 'https://github.com/adrianhajdin/threads',
+    //     linkType: 'Live Link',
+    // },
+    // {
+    //     iconUrl: car,
+    //     theme: 'btn-back-blue',
+    //     name: 'Car Finding App',
+    //     description: [
+    //         'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.'
+    //     ],
+    //     link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    //     linkType: 'Live Link',
+    // },
+    // {
+    //     iconUrl: snapgram,
+    //     theme: 'btn-back-pink',
+    //     name: 'Full Stack Instagram Clone',
+    //     description: [
+    //         'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.'
+    //     ],
+    //     link: 'https://github.com/adrianhajdin/social_media_app',
+    //     linkType: 'Live Link',
+    // },
+    // {
+    //     iconUrl: estate,
+    //     theme: 'btn-back-black',
+    //     name: 'Real-Estate Application',
+    //     description: [
+    //         'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.'
+    //     ],
+    //     link: 'https://github.com/adrianhajdin/projects_realestate',
+    //     linkType: 'Live Link',
+    // },
+    // {
+    //     iconUrl: summiz,
+    //     theme: 'btn-back-yellow',
+    //     name: 'AI Summarizer Application',
+    //     description: [
+    //         'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.'
+    //     ],
+    //     link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    //     linkType: 'Live Link',
+    // }
 ];
